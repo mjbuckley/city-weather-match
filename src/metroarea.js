43ef1c5c13245b61335921f29167b6b1e53e7702
Michael Buckley <michaelbuckleyis@gmail.com>
2017-01-21 21:18:19 -0500

    Create initial working version of the metro areas page.

--- src/metroarea.js
+++ src/metroarea.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+// An initial working pass at a metro areas page, but much to still do.  Issues:
+// -Currently does not distinguish between areas with same name (Columbus OH vs GA, etc.).
+// -Lists city names multiple times if multiple entries exist.
+// -Need to add links to the cities.
+// -Not sure if things are alphbatized or not.
+// -Assumes cities isn't empty. I don't think it could be, but make sure.
+// -What about handling jibberish input ex: /metro-areas/fakename?
+// -Should some of the checking be precomputed and put into weather.json?
 function MetroArea(props) {
+
+  const metroArea = props.params.metroarea;
+  let cities = [];
+
+  Object.keys(props.stationsObj).forEach(function(station) {
+    let areas = props.stationsObj[station]["area"];
+
+    if (areas.includes(metroArea)) {
+      cities.push(props.stationsObj[station]["city"]);
+    }
+  });
+
   return (
     <div>
-      Metro Area.
+      <h2>{metroArea} Urban Area</h2>
+      <ul>
+        {cities.map(function(city, index) {
+          return (<li key={index}>{city}</li>);
+        })}
+      </ul>
     </div>
   );
 }
--- src/sharedarealist.js
+++ src/sharedarealist.js
@@ -1,28 +1,67 @@
 import React from 'react';
+import { Link } from 'react-router'
 
+
+// Need to figure out the metro area disambiguation page and decide what props to pass there.
+// Also need to figure out how to deal with metro areas with the same name (ex: Columbus).
 // Iniitial test version. Eventually need to link to metro area disambiguation page.
 function SharedAreaList(props) {
 
-  return (
-    <div className="SharedAreaList">
+  if (props.sharedarea.length > 1) {
 
-      {(props.sharedarea.length > 1) ? (
-        <div>
-          <h4>{props.city} is part of the following urban areas:</h4>
-          <ul>
-            {props.sharedarea.map((area, index) => (
-              <li key={index}>{area}</li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <div>
-          <h4>{props.city} is part of the {props.sharedarea[0]} urban area</h4>
-        </div>
-      )}
-
-    </div>
-  );
+    return (
+      <div className="SharedAreaList">
+        <h4>{props.city} is part of the following urban areas:</h4>
+        <ul>
+          {props.sharedarea.map(function(area, index) {
+            let link = "/metro-areas/" + area;
+            return (<li key={index}><Link to={link}>{area}</Link></li>);
+            }
+          )}
+        </ul>
+      </div>
+    );
+  } else {
+    let link = "/metro-areas/" + props.sharedarea[0];
+    return (
+      <div className="SharedAreaList">
+        <h4>{props.city} is part of the <Link to={link}>{props.sharedarea[0]}</Link> urban area</h4>
+      </div>
+    );
+  }
 }
 
 export default SharedAreaList;
+
+// <ul>
+//   {props.sharedarea.map((area, index) => (
+//     <li key={index}><Link to="/fake">{area}</Link>}</li>
+//   ))}
+// </ul>
+
+
+// Below is a working version before I started messing with shared area links
+
+// function SharedAreaList(props) {
+//
+//   return (
+//     <div className="SharedAreaList">
+//
+//       {(props.sharedarea.length > 1) ? (
+//         <div>
+//           <h4>{props.city} is part of the following urban areas:</h4>
+//           <ul>
+//             {props.sharedarea.map((area, index) => (
+//               <li key={index}>{area}</li>
+//             ))}
+//           </ul>
+//         </div>
+//       ) : (
+//         <div>
+//           <h4>{props.city} is part of the {props.sharedarea[0]} urban area</h4>
+//         </div>
+//       )}
+//
+//     </div>
+//   );
+// }
