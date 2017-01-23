d96dd93ce825a550ecdea8aab6145ba977b3dc11
Michael Buckley <michaelbuckleyis@gmail.com>
2017-01-22 22:35:57 -0500

    Make basic working disambiguation page for cities with more than one station.

--- src/citylist.js
+++ src/citylist.js
@@ -9,8 +9,14 @@ class CityList extends Component {
     const city = this.props.stationsObj[this.props.station]["city"];
     const state = this.props.stationsObj[this.props.station]["state"];
     const station = this.props.station;
-    // Eventually make link different based on single or multiple locations for city
-    const link = "/location/" + city + "/" + state + "/" + station;
+
+    // Link to station info if only one station for city. Link to disambiguation page if There
+    // is more than one station in the city.
+    let link = "/location/" + city + "/" + state;
+
+    if (this.props.stationsObj[this.props.station]["multiCity"].length < 2) {
+      link = link + "/" + station;
+    }
 
     return (
       <li key={station} className="CityList">
--- src/citylocations.js
+++ src/citylocations.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router'
 
+
+// DEAL WITH SPACES IN CITY NAMES (maybe write stand alone function because I'll probably
+// use it in many places).
+// Deal with jibberish input
 function CityLocations(props) {
+
+  const city = props.params.city;
+  const state = props.params.state;
+  // const stationsObj = props.stationsObj;
+
+  // Find all stations in city
+  let locations = [];
+  Object.keys(props.stationsObj).forEach(function(station) {
+    if ( (props.stationsObj[station]["city"] === city) &&
+    (props.stationsObj[station]["state"] === state) ) {
+      locations.push(station);
+    }
+  });
+
   return (
     <div>
-      City Locations.
+
+      <h2>{city}, {state}</h2>
+
+      {(locations.length > 1) ? (
+        <p>There are several weather stations in {city}.</p>
+      ) : null}
+
+      <ul>
+        {locations.map(function(station) {
+          const link = "/location/" + city + "/" + state + "/" + station;
+          return (<li key={station}><Link to={link}>Station {station}</Link></li>);
+        })}
+      </ul>
     </div>
   );
 }
