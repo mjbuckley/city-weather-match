fda7206d31b2b20189da837934fd2b49e30e15e1
Michael Buckley <michaelbuckleyis@gmail.com>
2017-02-07 13:19:12 -0500

    Create initial working version of metro area page city links.

--- src/metroarea.js
+++ src/metroarea.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import buildLink from './buildlink.js';
 
 // LINKS RIGHT NOW ARE PLACEHOLDERS, NOT WORKING
 // An initial working pass at a metro areas page, but much to still do.  Issues:
@@ -10,37 +11,43 @@ import { Link } from 'react-router'
 // -Should some of the checking be precomputed and put into weather.json?
 
 const stationsObj = require('./data/weather.json');
+const metroMap = require('./data/metromap.json');
 
 function MetroArea(props) {
 
   const metroArea = decodeURIComponent(props.params.metroarea);
 
-  // Find cities that are in the metro area
-  let cities = [];
-  Object.keys(stationsObj).forEach(function(station) {
-    let areas = stationsObj[station]["area"];
-
-    if (areas.includes(metroArea)) {
-      cities.push(stationsObj[station]["city"]);
-    }
-  });
-
-  let previous = "";
-  let filteredCities = cities.filter(function(city) {
-    if (city === previous) {
-      return false;
-    } else {
-      previous = city;
-      return true;
-    }
-  });
+  // // Find cities that are in the metro area
+  // let cities = [];
+  // Object.keys(stationsObj).forEach(function(station) {
+  //   let areas = stationsObj[station]["area"];
+  //
+  //   if (areas.includes(metroArea)) {
+  //     cities.push(stationsObj[station]["city"]);
+  //   }
+  // });
+  //
+  // let previous = "";
+  // let filteredCities = cities.filter(function(city) {
+  //   if (city === previous) {
+  //     return false;
+  //   } else {
+  //     previous = city;
+  //     return true;
+  //   }
+  // });
+  const cities = Object.keys(metroMap[metroArea]);
 
   return (
     <div>
       <h2>{metroArea} Urban Area</h2>
       <ul>
-        {filteredCities.map(function(city, index) {
-          return (<li key={index}><Link to="/fake">{city}</Link></li>);
+        {cities.map(function(city, index) {
+          // this works, but ultimately be smarter about which station I link to, not just the first one.
+          const station = metroMap[metroArea][city][0];
+          const state = stationsObj[station]["state"];
+          const path ="/location/" + city + "/" + state + "/" + station;
+          return (<li key={index}><Link to={buildLink(props, path)}>{city}</Link></li>);
         })}
       </ul>
     </div>
