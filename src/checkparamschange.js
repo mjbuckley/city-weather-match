8c839c1597fd3a43bae393cd12f1e656e9944615
Michael Buckley <michaelbuckleyis@gmail.com>
2017-02-24 22:45:04 -0500

    Add lifecyle methods to MetroArea and fix infinite loop problem with checkparamschange.js.

--- src/checkparamschange.js
+++ src/checkparamschange.js
@@ -10,22 +10,56 @@ export default function checkParamsChange(nextProps, props) {
   // This can almost certainly be done better than this, but I need to look in to how JS handles
   // accessing non-existent object properties. Also, would be good to do this in a way to not need
   // to remember to alter this every time I add a weather value.
-  if (!nextProps.location.query ||
-      Object.keys(nextProps.location.query).length === 0 ||
-      !nextProps.location.query.maxTemp ||
-      nextProps.location.query.maxTemp !== props.location.query.maxTemp ||
-      !nextProps.location.query.lowTemp ||
+
+  if (!nextProps.location.query) { console.log("1");}
+    if(  Object.keys(nextProps.location.query).length === 0 ) { console.log("2");}
+      if(!nextProps.location.query.maxTemp ) { console.log("3");}
+      if(nextProps.location.query.maxTemp !== props.location.query.maxTemp ) { console.log("4");}
+      if(!nextProps.location.query.lowTemp ) { console.log("5");}
+      if(nextProps.location.query.lowTemp !== props.location.query.lowTemp ) { console.log("6");}
+      if(!nextProps.location.query.below32) { console.log("7");}
+      if(nextProps.location.query.below32 !== props.location.query.below32) { console.log("8");}
+      if(!nextProps.location.query.snowfall) { console.log("9");}
+      if(nextProps.location.query.snowfall !== props.location.query.snowfall) { console.log("10");}
+      if(!nextProps.location.query.precip) { console.log("11");}
+      if(nextProps.location.query.precip !== props.location.query.precip) { console.log("12");}
+
+  if (nextProps.location.query.maxTemp !== props.location.query.maxTemp ||
       nextProps.location.query.lowTemp !== props.location.query.lowTemp ||
-      !nextProps.location.query.below32 ||
       nextProps.location.query.below32 !== props.location.query.below32 ||
-      !nextProps.location.query.snow ||
-      nextProps.location.query.snow !== props.location.query.snow ||
-      !nextProps.location.query.precip ||
+      nextProps.location.query.snowfall !== props.location.query.snowfall ||
       nextProps.location.query.precip !== props.location.query.precip
   ) {
+    console.log("CPC");
     checkParams(nextProps);
   }
 }
+
+
+// Goodish 2/24
+
+// export default function checkParamsChange(nextProps, props) {
+//
+//   // This can almost certainly be done better than this, but I need to look in to how JS handles
+//   // accessing non-existent object properties. Also, would be good to do this in a way to not need
+//   // to remember to alter this every time I add a weather value.
+//   if (!nextProps.location.query ||
+//       Object.keys(nextProps.location.query).length === 0 ||
+//       !nextProps.location.query.maxTemp ||
+//       nextProps.location.query.maxTemp !== props.location.query.maxTemp ||
+//       !nextProps.location.query.lowTemp ||
+//       nextProps.location.query.lowTemp !== props.location.query.lowTemp ||
+//       !nextProps.location.query.below32 ||
+//       nextProps.location.query.below32 !== props.location.query.below32 ||
+//       !nextProps.location.query.snow ||
+//       nextProps.location.query.snow !== props.location.query.snow ||
+//       !nextProps.location.query.precip ||
+//       nextProps.location.query.precip !== props.location.query.precip
+//   ) {
+//     console.log("CPC");
+//     checkParams(nextProps);
+//   }
+// }
 
 // Convert nextProps to int?
 
