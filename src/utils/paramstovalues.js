d3dd646222d2415d0621740c1a317c11ccc3f336
Michael Buckley <michaelbuckleyis@gmail.com>
2017-04-07 16:59:06 -0400

    Update todo and some comments.

--- src/utils/paramstovalues.js
+++ src/utils/paramstovalues.js
@@ -1,40 +1,9 @@
-// import findMatches from './findmatches.js';
 import weatherOptions from '../data/weatheroptions.js';
 
 const minMax = require('../data/minmax.json');
 
-// This function is placed in the componentWillMount() sections of all direct children components of App (search,
-// results, etc.). It checks the URL for query parameters and then determines what App's state
-// should be. The URL is the source of truth. This allows for pages to be bookmarkable and it allows for the
-// back button to work more naturally (since previos state can be recreated from url). I bother with App state at all
-// for 2 reasons: 1) It acts as a sort of cache for matches because most of the time I will do some simple
-// calculations from the query params and determine that I can reuse the matches in state and not need to
-// recalculate everything, and 2) it generally makes passing around props easier to do (not impossible to do
-// otherwise, but it feels more natural).
-
-// NEW 3/5/17:
-// Takes props from App (which right now should only be query params) and returns an info object with complete/valid
-// weather values or false if such an object is not possible to create based on the query params.
-// SHOULD I ADD isActive to true in info?
-
-
-// HELPER VARIABLES (eventuall extract to stand alone).
-
-// Array of possible weather values
-// const weatherOptions = [ "mTmxAv", "mTmnAv", "andTmnLe32", "andSnGe1", "andPrGe5Ti"];
-
-// Maps weather terms I use in the app to weather terms used in minmax.json.
-// Ideally I should use the same terms in both places, but for now this is the fix.
-// const keyMap = {
-//   mTmxAv: "mTmxAv",
-//   mTmnAv: "mTmnAv",
-//   andTmnLe32: "andTmnLe32",
-//   andSnGe1: "andSnGe1",
-//   andPrGe5Ti: "andPrGe5Ti"
-// };
-
-// Default values are the values that are the extreme min or max of the possible range,
-// and the value is such that it would exclude no station.
+// The value (either max or min possible) for each weather option such no station is excluded by that value.
+// Used to fill in valid but partially empty query params.
 const defaults = {
   mTmxAv: minMax["mTmxAv"][1],
   mTmnAv: minMax["mTmnAv"][0],
@@ -45,14 +14,8 @@ const defaults = {
 };
 
 
-// THERE SEEMS TO BE AN ISSUE WHERE IF PICKS UP THE QUERY BUT DOESN'T MAKE IT
-// INTO THE INFO.LENGTH>0 IF SECTION.
-
-
-
-
 // Function takes query param values, validates, and returns a full set of weather values if valid
-// ane returns {isActive: false} if invalid. Function assumes that at least some query params are present,
+// and returns {isActive: false} if invalid. Function assumes that at least some query params are present,
 // but not that they are valid (checks for presence should be done before this function is called.)
 export default function paramsToValues(props) {
 
