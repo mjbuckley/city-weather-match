76fa9aceeac3c917c2e9bced0a36d88111b41b52
Michael Buckley <michaelbuckleyis@gmail.com>
2017-05-26 18:10:11 -0400

    Convert search form to use fieldset and legend.

--- src/components/weatherrangeinput.js
+++ src/components/weatherrangeinput.js
@@ -7,9 +7,8 @@ function WeatherRangeInput(props) {
 
   return (
     <div className="WeatherRangeInput">
-      <label className="WeatherRangeInput-label" htmlFor={props.id}>{props.description}</label>
       <div className="WeatherRangeInput-input-wrap">
-        <span>At Least:</span>
+        <label htmlFor={props.id}>{props.label}</label>
         <input
           className="WeatherRangeInput-input"
           type="range"
@@ -29,3 +28,36 @@ function WeatherRangeInput(props) {
 }
 
 export default WeatherRangeInput;
+
+
+// import React from 'react';
+// import '../css/weatherrangeinput.css';
+//
+// // Component for a range input slider inside of a form. Min and max should come from minmax.json
+// // function WeatherRangeInput(props) {
+// function WeatherRangeInput(props) {
+//
+//   return (
+//     <div className="WeatherRangeInput">
+//       <label className="WeatherRangeInput-label" htmlFor={props.id}>{props.description}</label>
+//       <div className="WeatherRangeInput-input-wrap">
+//         <span>At Least:</span>
+//         <input
+//           className="WeatherRangeInput-input"
+//           type="range"
+//           id={props.id}
+//           min={props.min}
+//           max={props.max}
+//           step="1"
+//           value={props.value}
+//           onChange={props.onChange}
+//         />
+//         <div className="WeatherRangeInput-unit-value">
+//           {props.value}<span>{(props.unit ? (" " + props.unit) : "")}</span>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+//
+// export default WeatherRangeInput;
