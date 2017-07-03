dd44a5641dea0177953cbf6c2f90d85aa427d34b
Michael Buckley <michaelbuckleyis@gmail.com>
2017-07-02 21:25:01 -0400

    Initial attempt at a range slider fill.

--- src/components/weatherrangeinput.js
+++ src/components/weatherrangeinput.js
@@ -5,19 +5,30 @@ import '../css/weatherrangeinput.css';
 // function WeatherRangeInput(props) {
 function WeatherRangeInput(props) {
 
+  // Below is used to calculate width for the fake slider fill.
+  const range = props.max - props.min;
+  const distFromMin = props.value - props.min;
+  const percentage = distFromMin * 100 / range;
+  const fillValue = percentage + "%";
+  const widthStyle = { width: fillValue };
+
   return (
     <div className="WeatherRangeInput">
       <label htmlFor={props.id}>{props.label}</label>
-      <input
-        className="WeatherRangeInput-input"
-        type="range"
-        id={props.id}
-        min={props.min}
-        max={props.max}
-        step="1"
-        value={props.value}
-        onChange={props.onChange}
-      />
+      <div className="outer-container">
+        <div className="inner-container" style={widthStyle}></div>
+        <div className="inner-container2"></div>
+        <input
+          className="WeatherRangeInput-input"
+          type="range"
+          id={props.id}
+          min={props.min}
+          max={props.max}
+          step="1"
+          value={props.value}
+          onChange={props.onChange}
+        />
+      </div>
       <div className="WeatherRangeInput-unit-value">
         <div className="value">{props.value}</div>
         <div className="unit">{props.unit}</div>
@@ -27,3 +38,28 @@ function WeatherRangeInput(props) {
 }
 
 export default WeatherRangeInput;
+
+
+// Good/old july 1:
+// function WeatherRangeInput(props) {
+//
+//   return (
+//     <div className="WeatherRangeInput">
+//       <label htmlFor={props.id}>{props.label}</label>
+//       <input
+//         className="WeatherRangeInput-input"
+//         type="range"
+//         id={props.id}
+//         min={props.min}
+//         max={props.max}
+//         step="1"
+//         value={props.value}
+//         onChange={props.onChange}
+//       />
+//       <div className="WeatherRangeInput-unit-value">
+//         <div className="value">{props.value}</div>
+//         <div className="unit">{props.unit}</div>
+//       </div>
+//     </div>
+//   );
+// }
