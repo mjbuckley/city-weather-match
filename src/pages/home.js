d8d703baf489dac737eeb97504631f4d51578387
Michael Buckley <michaelbuckleyis@gmail.com>
2017-07-30 21:29:20 -0400

    Create basic layout for front page landing.

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,9 +8,13 @@ function Home(props) {
 
       <div className="home-wrapper">
         <div className="home">
-          <h2>Home Page Info Here</h2>
-          <p>There will be text and images here explaining the site</p>
-          <p>More text and exciting stuff!</p>
+          <div className="fake-image">Image goes here</div>
+          <h2>City Weather Match</h2>
+          <p>Enter you're weather preferences and search nearly 4000 locations to find the cities that match the type of weather that you want.</p>
+          <div className="home-start-box">
+            <p className="home-start">Start Searching Below</p>
+            <p className="home-arrow">▼</p>
+          </div>
         </div>
       </div>
 
