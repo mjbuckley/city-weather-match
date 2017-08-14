cc7ec8fcc2a03e8cdca4e0947a52129b9ea08ad2
Michael Buckley <michaelbuckleyis@gmail.com>
2017-08-13 23:10:17 -0400

    Improve site text.

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,7 +17,7 @@ function Home(props) {
         <div className="home">
           <HomeImage className="home-image"/>
           <h2>City Weather Match</h2>
-          <p>Find the cities where the typical weather matches your personal preferrences. Just enter your desired weather below to search thousands of locations and find the cities that are a match for you.</p>
+          <p>Find the cities that match your personal weather preferrences. Just enter your desired weather below to search from thousands of locations and find the cities that are a match for you.</p>
           <div className="home-start-box">
             <p className="home-start">Start Searching Below</p>
             <p className="home-arrow">▼</p>
